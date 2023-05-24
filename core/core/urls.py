9417957da22a5eccc1b61becaa5e015a02437425
eskazemi <smk182018@gmail.com>
2023-05-24 16:03:52 +0330

    Feat(): display products and detail product
    -create get_absolute_url function in product model
    -create templates for product app
    -create urls.py for product app
    -add url realted to product

--- core/core/urls.py
+++ core/core/urls.py
@@ -3,12 +3,15 @@ from django.urls import (
     path,
     include,
 )
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include("accounts.urls", namespace="accounts")),
     path('home/', include("home.urls", namespace="home")),
-]
+    path('product/', include("product.urls", namespace="product")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = 'Ecommerco'
 admin.site.site_name = 'Ecommerco'
--- core/product/views.py
+++ core/product/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render,
+    get_object_or_404,
+)
+from django.views import View
+from .models import Product
 
-# Create your views here.
+
+class ProductView(View):
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.filter(is_available=True)
+        return render(request, 'product/index.html', {"products": products})
+
+
+class ProductDetailView(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        product = get_object_or_404(Product, slug=slug)
+        return render(request, 'product/detail.html', {'product': product})
+
