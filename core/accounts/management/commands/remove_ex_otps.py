755ef3a7af3ba942f7f2dfc6800a791060a06bc8
eskazemi <smk182018@gmail.com>
2023-08-13 16:36:43 +0330

    Feat(): command management
    -create command ->act = remove all expired otp code

---
+++ core/accounts/management/commands/remove_ex_otps.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from accounts.models import OtpCode
+from datetime import (
+    datetime,
+    timedelta,
+)
+import pytz
+
+
+class Command(BaseCommand):
+    help = "remove all expired otp codes"
+
+    def handle(self, *args, **options):
+        expired_time = datetime.now().replace(tzinfo=pytz.UTC) - timedelta(minutes=2)
+        OtpCode.objects.filter(created_at__lt=expired_time).delete()
+
+        self.stdout.write(self.style.SUCCESS("successfully remove all otp codes"))
