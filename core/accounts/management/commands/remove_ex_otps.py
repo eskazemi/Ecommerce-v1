9444623912c675bd53e6f15be4828bf100b78d54
eskazemi <smk182018@gmail.com>
2023-08-18 21:12:49 +0330

    Fix():set tzinfo

--- core/accounts/management/commands/remove_ex_otps.py
+++ core/accounts/management/commands/remove_ex_otps.py
@@ -11,7 +11,7 @@ class Command(BaseCommand):
     help = "remove all expired otp codes"
 
     def handle(self, *args, **options):
-        expired_time = datetime.now().replace(tzinfo=pytz.UTC) - timedelta(minutes=2)
+        expired_time = datetime.now().replace(tzinfo=pytz.timezone('Asia/Tehran')) - timedelta(minutes=2)
         OtpCode.objects.filter(created_at__lt=expired_time).delete()
 
         self.stdout.write(self.style.SUCCESS("successfully remove all otp codes"))
