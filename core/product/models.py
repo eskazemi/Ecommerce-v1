b899695709689cfb00c494fb077d6bd47c4cf0e4
eskazemi <smk182018@gmail.com>
2023-08-25 10:29:48 +0330

    fix(): add __str__ function

--- core/product/models.py
+++ core/product/models.py
@@ -35,5 +35,8 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.name
+
     def get_absolute_url(self):
         return reverse('product:product_detail', args=[self.slug])
